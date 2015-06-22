6e520705830efb3ccb498435118e3b440264542d
Basarat Syed <basaratali@gmail.com>
2015-06-22 17:27:33 +1000

    kick travis

--- release.sh
+++ release.sh
@@ -11,6 +11,9 @@ toolsVersion="2"
 
 commitName="1.$(date +%Y%m%d%H%M).$toolsVersion+$commitHash"
 
+# Kick travis
+echo '$commitName' > kicktravis
+
 # Update package.json
 < package.json > package.json.new sed -E "s/(\s+\"version\": \")[^\"]+(\",)/\1$commitName\2/"
 mv package.json.new package.json
@@ -18,7 +21,7 @@ echo "Adding to git"
 git add -A
 
 # Commit,tag,push,publish
-git commit -m $commitName --allow-empty
+git commit -m $commitName
 git tag $commitName
 git push
 git push --tags
