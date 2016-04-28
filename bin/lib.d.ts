bb55a6d655ec5499d41c9ad71cda5b16af1b8e73
Travis-CI <basaratali@gmail.com>
2016-04-28 22:08:43 +0000

    2016-04-28 [ci skip] Version: 1.201604282208.1+4ebf4880e5ba28b8f39d7a55dd26e330349e165e

--- bin/lib.d.ts
+++ bin/lib.d.ts
@@ -231,7 +231,6 @@ interface Function {
       * @param thisArg The object to be used as the this object.
       * @param argArray A set of arguments to be passed to the function.
       */
-    apply<T, U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, argArray?: any): U;
     apply(this: Function, thisArg: any, argArray?: any): any;
 
     /**
@@ -239,7 +238,6 @@ interface Function {
       * @param thisArg The object to be used as the current object.
       * @param argArray A list of arguments to be passed to the method.
       */
-    call<T, U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, ...argArray: any[]): U;
     call(this: Function, thisArg: any, ...argArray: any[]): any;
 
     /**
@@ -248,7 +246,6 @@ interface Function {
       * @param thisArg An object to which the this keyword can refer inside the new function.
       * @param argArray A list of arguments to be passed to the new function.
       */
-    bind<T, U>(this: (this: T, ...argArray: any[]) => U, thisArg: T, ...argArray: any[]): (this: void, ...argArray: any[]) => U;
     bind(this: Function, thisArg: any, ...argArray: any[]): any;
 
     prototype: any;
